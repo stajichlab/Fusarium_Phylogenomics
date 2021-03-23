c74ea91b0209cda722ff3100bac3dd774292dbe3
Jason Stajich <jasonstajich.phd@gmail.com>
2021-03-23 15:58:51 -0700

    unzip, realign with two data files

--- pipeline/01_download.sh
+++ pipeline/01_download.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/bash
-#SBATCH -p short --out logs/download.%a.log
+#SBATCH -p short --out logs/download.log
 module load aspera
-#DAT=(lib/ncbi_accessions_Fusarium.csv lib/ncbi_accessions_outgroups.csv)
-DAT=(lib/ncbi_accessions_outgroups.csv)
-#ACCESSION,SPECIES,STRAIN,NCBI_TAXID,BIOPROJECT,N50,ASM_NAME
+DAT=(lib/ncbi_accessions_Fusarium.csv lib/ncbi_accessions_outgroups.csv)
 OUT=source/NCBI_ASM
+STAGE=genomes
+mkdir -p $STAGE
 IFS=,
-for file in ${DAT[@]}; 
+for file in ${DAT[@]};
 do
-    tail -n +2 $file | while read ACCESSION SPECIES STRAIN TAXID BIOPROJECT N50 ASMNAME
-    do
-	PRE=$(echo $ACCESSION | cut -d_ -f1 )
-	ONE=$(echo $ACCESSION | cut -d_ -f2 | awk '{print substr($1,1,3)}')
-	TWO=$(echo $ACCESSION | cut -d_ -f2 | awk '{print substr($1,4,3)}')
-	THREE=$(echo $ACCESSION | cut -d_ -f2 | awk '{print substr($1,7,3)}')
-	ASMNAME=$(echo $ASMNAME | perl -p -e 's/ /_/g')
-	echo "anonftp@ftp.ncbi.nlm.nih.gov:/genomes/all/$PRE/$ONE/$TWO/$THREE/${ACCESSION}_$ASMNAME/"
-	if [ ! -d $OUT/${ACCESSION}_$ASMNAME ]; then
-	    ascp -k1 -Tdr -l400M -i $ASPERAKEY --overwrite=diff anonftp@ftp.ncbi.nlm.nih.gov:/genomes/all/$PRE/$ONE/$TWO/$THREE/${ACCESSION}_$ASMNAME ./$OUT/
-	fi	
-    done
+  echo "file is $file"
+  tail -n +2 $file | while read ACCESSION SPECIES STRAIN TAXID BIOPROJECT ASMLEN N50 ASMNAME
+  do
+    OUTNAME=$(echo "${SPECIES}_$STRAIN" | perl -p -e 's/\s+$//; s/[\s\/\;]/_/g')
+    PRE=$(echo $ACCESSION | cut -d_ -f1 )
+    ONE=$(echo $ACCESSION | cut -d_ -f2 | awk '{print substr($1,1,3)}')
+    TWO=$(echo $ACCESSION | cut -d_ -f2 | awk '{print substr($1,4,3)}')
+    THREE=$(echo $ACCESSION | cut -d_ -f2 | awk '{print substr($1,7,3)}')
+    ASMNAME=$(echo $ASMNAME | perl -p -e 's/ /_/g')
+    echo "anonftp@ftp.ncbi.nlm.nih.gov:/genomes/all/$PRE/$ONE/$TWO/$THREE/${ACCESSION}_${ASMNAME}/"
+    if [ ! -d $OUT/${ACCESSION}_$ASMNAME ]; then
+      ascp -k1 -Tdr -l400M -i $ASPERAKEY --overwrite=diff anonftp@ftp.ncbi.nlm.nih.gov:/genomes/all/$PRE/$ONE/$TWO/$THREE/${ACCESSION}_$ASMNAME ./$OUT/
+    fi
+    if [ ! -s $STAGE/${OUTNAME}.dna.fasta ]; then
+      pigz -dc $OUT/${ACCESSION}_${ASMNAME}/${ACCESSION}_${ASMNAME}_genomic.fna.gz > $STAGE/${OUTNAME}.dna.fasta
+    fi
+  done
 done
