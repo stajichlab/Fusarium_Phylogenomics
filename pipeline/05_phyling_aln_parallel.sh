c587b369f5b571affd6558c3bc169d07f54f5c4b
Jason Stajich <jasonstajich.phd@gmail.com>
2021-03-31 00:16:42 -0700

    PHYling steps

---
+++ pipeline/05_phyling_aln_parallel.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+#SBATCH --ntasks 32 --mem 24G --time 2:00:00 -p short -N 1
+module load parallel
+module load hmmer/3
+
+module load miniconda3
+if [ ! -f config.txt ]; then
+    echo "Need config.txt for PHYling"
+    exit
+fi
+
+source config.txt
+
+if [ ! -z $PREFIX ]; then
+    rm -rf aln/$PREFIX
+fi
+# probably should check to see if allseq is newer than newest file in the folder?
+echo " I will remove prefix.tab to make sure it is regenerated"
+#rm prefix.tab
+./PHYling_unified/PHYling aln -c -q parallel
