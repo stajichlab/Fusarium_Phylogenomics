5008f0f74443f12684ef7b27bf79d0179066b359
Jason Stajich <jasonstajich.phd@gmail.com>
2021-03-30 20:30:01 -0700

    specify folder for chmod

--- scripts/get_ncbi_datasets.sh
+++ scripts/get_ncbi_datasets.sh
@@ -7,7 +7,7 @@ if [ ! -f $BINDIR/datasets ]; then
 	curl -o $BINDIR/dataformat https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/dataformat
 	curl -o $BINDIR/datasets https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets
 
-	chmod +x dataformat datasets
+	chmod +x $BINDIR/dataformat $BINDIR/datasets
 fi
 ACCESSION=lib/ncbi_accessions.json
 INGROUP=lib/ingroups.NCBI.csv
